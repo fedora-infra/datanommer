42e7779fc9a5b49fe8ab6717389e3bdfb3f72982
Ralph Bean <rbean@redhat.com>
2012-08-31 21:54:18 -0400

    Don't setup the DB model if we're not configured to enable.

--- datanommer/consumer.py
+++ datanommer/consumer.py
@@ -18,6 +18,12 @@ class Nommer(fedmsg.consumers.FedmsgConsumer):
     def __init__(self, hub):
         super(Nommer, self).__init__(hub)
 
+        # If fedmsg doesn't think we should be enabled, then we should quit
+        # before setting up all the extra special zmq machinery.
+        # __initialized is set in moksha.api.hub.consumer
+        if not getattr(self, "__initialized", False):
+            return
+
         # Setup a sqlalchemy DB connection (postgres, or sqlite)
         datanommer.models.init(self.hub.config['datanommer.sqlalchemy.url'])
 
