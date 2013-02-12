7db673816939a8476e461adfcca6c3d60f876b99
Ralph Bean <rbean@redhat.com>
2013-02-12 12:55:47 -0500

    Fix that example relay config.

--- fedmsg.d/endpoints.py
+++ fedmsg.d/endpoints.py
@@ -1,6 +1,6 @@
 config = dict(
     endpoints={
-        "relay_outbound": ["tcp://*:4001"],
+        "relay_outbound": ["tcp://127.0.0.1:4001"],
     },
     relay_inbound="tcp://127.0.0.1:2003",
 )
