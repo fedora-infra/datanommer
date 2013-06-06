3f27a4b9722cd9723a7489447300b6db86de924d
Ralph Bean <rbean@redhat.com>
2013-06-06 10:31:05 -0400

    0.4.6

--- datanommer.models/setup.py
+++ datanommer.models/setup.py
@@ -9,7 +9,7 @@ long_description = f.read().strip()
 long_description = long_description.split('split here', 1)[1]
 f.close()
 
-version = '0.4.5'
+version = '0.4.6'
 
 setup(name='datanommer.models',
       version=version,
