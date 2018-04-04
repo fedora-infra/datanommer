fc5d5ca748f4bed7a8aa13603879f328b2b8dcae
Ralph Bean <rbean@redhat.com>
2018-04-04 13:27:28 -0400

    datanommer.consumer-0.8.1

--- datanommer.consumer/setup.py
+++ datanommer.consumer/setup.py
@@ -21,7 +21,7 @@ long_description = f.read().strip()
 long_description = long_description.split('split here', 1)[1]
 f.close()
 
-version = '0.8.0'
+version = '0.8.1'
 
 setup(
     name='datanommer.consumer',
