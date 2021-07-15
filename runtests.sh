37df00b05cf2bc93182ac5127c2539d2d9a4f744
Ryan Lerch <rlerch@redhat.com>
2021-07-15 11:48:14 +1000

    Add check in runtests to see if krb5-devel (krb5-config) is installed
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- runtests.sh
+++ runtests.sh
@@ -10,6 +10,10 @@ which pre-commit &>/dev/null || {
     echo "You need to install pre-commit" >&2
     exit 2
 }
+which krb5-config &> /dev/null || {
+    echo "You need to install krb5-devel" >&2
+    exit 2
+}
 
 echo "Running checks for all packages"
 pre-commit run --all-files
