ef7d11cbb65ea27b05dd20fc4b748d7c1fdecb99
Aurélien Bompard <aurelien@bompard.org>
2021-07-23 16:35:14 +0200

    And now fix liccheck
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- tools/run-liccheck.sh
+++ tools/run-liccheck.sh
@@ -10,6 +10,7 @@ TMPFILE=$(mktemp -t requirements-XXXXXX.txt)
 # flake8 requires importlib_metadata on python < 3.8, so it's not installed, but it's exported
 # and liccheck crashes on packages listed in the req file but not installed.
 # poetry export --dev -f requirements.txt -o $TMPFILE
+poetry export --dev --without-hashes -f requirements.txt -o $TMPFILE
 
-pip freeze --exclude-editable --isolated > $TMPFILE
+#pip freeze --exclude-editable --isolated > $TMPFILE
 liccheck -r $TMPFILE
