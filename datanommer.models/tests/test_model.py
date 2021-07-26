fd554cb7d2fc48c83b9d6801d60b652baa59795d
Ryan Lerch <rlerch@redhat.com>
2021-07-26 17:41:28 +1000

    Add back tests to test_models abompard noted
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- datanommer.models/tests/test_model.py
+++ datanommer.models/tests/test_model.py
@@ -13,7 +13,9 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
+import datetime
 
+from bodhi.messages.schemas.update import UpdateCommentV1
 from fedora_messaging import message as fedora_message
 
 from datanommer.models import add, Message, session
@@ -23,6 +25,71 @@ def generate_message(
     topic="nice.message", body={"encouragement": "You're doing great!"}, headers=None
 ):
     return fedora_message.Message(topic=topic, body=body, headers=headers)
+
+
+def generate_bodhi_update_complete_message(text="testing testing"):
+    msg = UpdateCommentV1(
+        body={
+            "comment": {
+                "karma": -1,
+                "text": text,
+                "timestamp": "2019-03-18 16:54:48",
+                "update": {
+                    "alias": "FEDORA-EPEL-2021-f2d195dada",
+                    "builds": [
+                        {"nvr": "abrt-addon-python3-2.1.11-50.el7"},
+                        {"nvr": "kernel-10.4.0-2.el7"},
+                    ],
+                    "status": "pending",
+                    "release": {"name": "F35"},
+                    "request": "testing",
+                    "user": {"name": "ryanlerch"},
+                },
+                "user": {"name": "dudemcpants"},
+            }
+        }
+    )
+    msg.topic = f"org.fedoraproject.stg.{msg.topic}"
+    return msg
+
+
+def test_add_missing_timestamp(datanommer_models):
+    example_message = generate_message()
+    example_message._properties.headers["sent-at"] = None
+
+    add(example_message)
+
+    dbmsg = Message.query.first()
+    timediff = datetime.datetime.utcnow() - dbmsg.timestamp
+    # 10 seconds between adding the message and checking
+    # the timestamp should be more than enough.
+    assert timediff < datetime.timedelta(seconds=10)
+
+
+def test_add_and_check_for_others(datanommer_models):
+    def _count_array_attr(name):
+        return Message.get_array(name).count()
+
+    # There are no users or packages at the start
+    assert _count_array_attr("users") == 0
+    assert _count_array_attr("packages") == 0
+
+    # Then add a message
+    add(generate_bodhi_update_complete_message())
+
+    # There should now be two of each
+    assert _count_array_attr("users") == 2
+    assert _count_array_attr("packages") == 2
+
+    # If we add it again, there should be no duplicates
+    add(generate_bodhi_update_complete_message())
+    assert _count_array_attr("users") == 2
+    assert _count_array_attr("packages") == 2
+
+    # Add a new username
+    add(generate_bodhi_update_complete_message(text="this is @abompard in a comment"))
+    assert _count_array_attr("users") == 3
+    assert _count_array_attr("packages") == 2
 
 
 def test_add_nothing(datanommer_models):
@@ -36,7 +103,7 @@ def test_add_and_check(datanommer_models):
 
 
 def test_categories(datanommer_models):
-    add(generate_message(topic="org.fedoraproject.prod.bodhi.newupdate"))
+    add(generate_bodhi_update_complete_message())
     session.flush()
     obj = Message.query.first()
     assert obj.category == "bodhi"
@@ -79,6 +146,18 @@ def test_grep_not_category(datanommer_models):
     assert t == 0
     assert p == 0
     assert len(r) == 0
+
+
+def test_add_headers(datanommer_models):
+    example_headers = {"foo": "bar", "baz": 1, "wibble": ["zork", "zap"]}
+    example_message = generate_message(
+        topic="org.fedoraproject.prod.bodhi.newupdate", headers=example_headers
+    )
+    add(example_message)
+    dbmsg = Message.query.first()
+    assert dbmsg.headers["foo"] == "bar"
+    assert dbmsg.headers["baz"] == 1
+    assert dbmsg.headers["wibble"] == ["zork", "zap"]
 
 
 def test_grep_topics(datanommer_models):
