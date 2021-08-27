bf392e31be0a039750a7b4b51a9193ba005c3e64
Aurélien Bompard <aurelien@bompard.org>
2021-08-27 17:21:35 +0200

    Minor comment fixes to the run-liccheck script
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- tools/run-liccheck.sh
+++ tools/run-liccheck.sh
@@ -3,14 +3,13 @@
 trap 'rm -f "$TMPFILE"' EXIT
 
 set -e
+set -x
 
 TMPFILE=$(mktemp -t requirements-XXXXXX.txt)
 
-# Note: we can't use poetry export because it isn't smart enough with conditional dependencies:
-# flake8 requires importlib_metadata on python < 3.8, so it's not installed, but it's exported
-# and liccheck crashes on packages listed in the req file but not installed.
-# poetry export --dev -f requirements.txt -o $TMPFILE
 poetry export --dev --without-hashes -f requirements.txt -o $TMPFILE
 
+# Use pip freeze instead of poetry when it fails
 #pip freeze --exclude-editable --isolated > $TMPFILE
+
 liccheck -r $TMPFILE
