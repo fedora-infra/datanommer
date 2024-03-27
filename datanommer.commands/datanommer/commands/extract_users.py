320a4667d9ec04077e18514d7ac6acec7b8ce362
Aurélien Bompard <aurelien@bompard.org>
2024-03-27 17:51:27 +0100

    Add a command to extract users from existing messages and insert them
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ datanommer.commands/datanommer/commands/extract_users.py
@@ -0,0 +1,77 @@
+import logging
+
+import click
+from fedora_messaging.message import load_message
+from sqlalchemy import and_
+
+import datanommer.models as m
+
+from . import config_option, get_config
+
+
+log = logging.getLogger(__name__)
+
+
+@click.command()
+@config_option
+@click.option(
+    "--topic", default=None, help="Only extract users for messages of a specific topic."
+)
+@click.option(
+    "--category",
+    default=None,
+    help="Only extract users for messages of a specific category.",
+)
+def main(config_path, topic, category):
+    """Go over old messages, extract users and store them.
+
+    This is useful when a message schema has been added and we want to populate the users table
+    with the new information.
+    """
+    config = get_config(config_path)
+    m.init(
+        config["datanommer_sqlalchemy_url"],
+        alembic_ini=config["alembic_ini"],
+    )
+
+    if topic and category:
+        raise click.UsageError("can't use both --topic and --category, choose one.")
+
+    query = m.Message.query
+    if topic:
+        query = query.filter(m.Message.topic == topic)
+    elif category:
+        query = query.filter(m.Message.category == category)
+
+    query = (
+        query.join(
+            m.users_assoc_table,
+            and_(
+                m.Message.id == m.users_assoc_table.c.msg_id,
+                m.Message.timestamp == m.users_assoc_table.c.msg_timestamp,
+            ),
+            isouter=True,
+        )
+        .filter(m.users_assoc_table.c.msg_id.is_(None))
+        .order_by(m.Message.timestamp)
+    )
+
+    for message in query:
+        fm_msg = load_message(
+            {
+                "topic": message.topic,
+                "headers": message.headers,
+                "id": message.msg_id,
+                "body": message.msg,
+            }
+        )
+        usernames = fm_msg.usernames
+        if not usernames:
+            continue
+        message._insert_list(m.User, m.users_assoc_table, usernames)
+        log.info(
+            "Usernames for message %s of topic %s: %s",
+            message.msg_id,
+            message.topic,
+            ", ".join(usernames),
+        )
---
+++ datanommer.commands/tests/test_extract_users.py
@@ -0,0 +1,99 @@
+import pytest
+import sqlalchemy as sa
+from click.testing import CliRunner
+
+import datanommer.models as m
+from datanommer.commands.extract_users import main as extract_users
+
+from .utils import generate_bodhi_update_complete_message, generate_message
+
+
+@pytest.fixture
+def bodhi_message_db(datanommer_models):
+    msg = generate_bodhi_update_complete_message()
+    m.add(msg)
+    m.session.execute(m.users_assoc_table.delete())
+    m.session.commit()
+
+    msg_in_db = m.Message.from_msg_id(msg.id)
+    assert len(msg_in_db.users) == 0
+    return msg_in_db
+
+
+def test_extract_users(bodhi_message_db, mock_config, mock_init):
+    runner = CliRunner()
+    result = runner.invoke(extract_users)
+
+    assert result.exit_code == 0, result.output
+
+    m.session.refresh(bodhi_message_db)
+    assert len(bodhi_message_db.users) > 0
+    assert {u.name for u in bodhi_message_db.users} == {"dudemcpants", "ryanlerch"}
+
+
+def test_extract_users_topic(bodhi_message_db, mock_config, mock_init):
+    runner = CliRunner()
+    result = runner.invoke(
+        extract_users, ["--topic", "org.fedoraproject.stg.bodhi.update.comment"]
+    )
+
+    assert result.exit_code == 0, result.output
+
+    m.session.refresh(bodhi_message_db)
+    assert len(bodhi_message_db.users) > 0
+    assert {u.name for u in bodhi_message_db.users} == {"dudemcpants", "ryanlerch"}
+
+
+def test_extract_users_wrong_topic(bodhi_message_db, mock_config, mock_init):
+    runner = CliRunner()
+    result = runner.invoke(extract_users, ["--topic", "something.else"])
+
+    assert result.exit_code == 0, result.output
+
+    m.session.refresh(bodhi_message_db)
+    assert len(bodhi_message_db.users) == 0
+
+
+def test_extract_users_category(bodhi_message_db, mock_config, mock_init):
+    runner = CliRunner()
+    result = runner.invoke(extract_users, ["--category", "bodhi"])
+
+    assert result.exit_code == 0, result.output
+
+    m.session.refresh(bodhi_message_db)
+    assert len(bodhi_message_db.users) > 0
+    assert {u.name for u in bodhi_message_db.users} == {"dudemcpants", "ryanlerch"}
+
+
+def test_extract_users_wrong_category(bodhi_message_db, mock_config, mock_init):
+    runner = CliRunner()
+    result = runner.invoke(extract_users, ["--category", "git"])
+
+    assert result.exit_code == 0, result.output
+
+    m.session.refresh(bodhi_message_db)
+    assert len(bodhi_message_db.users) == 0
+
+
+def test_extract_users_topic_and_category(mock_config, mock_init):
+    runner = CliRunner()
+    result = runner.invoke(
+        extract_users, ["--category", "bodhi", "--topic", "some.topic"]
+    )
+    print(result.output)
+    assert result.exit_code != 0, result.output
+    assert "Error: can't use both --topic and --category, choose one." in result.output
+
+
+def test_extract_users_no_users(datanommer_models, mock_config, mock_init):
+    msg = generate_message()
+    m.add(msg)
+    runner = CliRunner()
+    result = runner.invoke(extract_users)
+
+    assert result.exit_code == 0, result.output
+    users_count = m.session.execute(
+        sa.select(sa.func.count(m.users_assoc_table.c.msg_id))
+    ).scalar_one()
+    assert users_count == 0
+    assert result.output == ""
