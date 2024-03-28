601440e4b157b6005ee2ae605fcb02d9b3dcc3de
Aurélien Bompard <aurelien@bompard.org>
2024-03-28 18:34:33 +0100

    Fix the extract_users script and add an option to force the schema
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- datanommer.commands/datanommer/commands/extract_users.py
+++ datanommer.commands/datanommer/commands/extract_users.py
@@ -2,7 +2,7 @@ import logging
 
 import click
 from fedora_messaging.message import load_message
-from sqlalchemy import and_, select
+from sqlalchemy import and_, func, select
 
 import datanommer.models as m
 
@@ -22,7 +22,15 @@ log = logging.getLogger(__name__)
     default=None,
     help="Only extract users for messages of a specific category.",
 )
-def main(config_path, topic, category):
+@click.option(
+    "--force-schema",
+    default=None,
+    help=(
+        "Force usage of this schema name to extract usernames. This is the key in the "
+        "exposed entry point / plugin, for example: wiki.article.edit.v1"
+    ),
+)
+def main(config_path, topic, category, force_schema):
     """Go over old messages, extract users and store them.
 
     This is useful when a message schema has been added and we want to populate the users table
@@ -56,11 +64,21 @@ def main(config_path, topic, category):
         .order_by(m.Message.timestamp)
     )
 
+    total = m.session.scalar(query.with_only_columns(func.count(m.Message.id)))
+    if not total:
+        click.echo("No messages matched.")
+        return
+
+    click.echo(f"Considering {total} messages")
+
     for message in m.session.scalars(query):
+        headers = message.headers
+        if force_schema:
+            headers["fedora_messaging_schema"] = force_schema
         fm_msg = load_message(
             {
                 "topic": message.topic,
-                "headers": message.headers,
+                "headers": headers,
                 "id": message.msg_id,
                 "body": message.msg,
             }
@@ -69,9 +87,8 @@ def main(config_path, topic, category):
         if not usernames:
             continue
         message._insert_list(m.User, m.users_assoc_table, usernames)
-        log.info(
-            "Usernames for message %s of topic %s: %s",
-            message.msg_id,
-            message.topic,
-            ", ".join(usernames),
+        click.echo(
+            f"Usernames for message {message.msg_id} of topic {message.topic}"
+            f": {', '.join(usernames)}"
         )
+    m.session.commit()
